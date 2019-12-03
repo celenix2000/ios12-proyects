ffe5e1a36f6d1e2a29d5a62bf24df658413a412c
celenix2000 <58379037+celenix2000@users.noreply.github.com>
2019-12-02 23:23:22 -0600

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Felipe Neri Bautista Bautista on 02/12/19.
+//  Copyright © 2019 Bautista Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
